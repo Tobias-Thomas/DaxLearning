ffa7d7e6c20b838478434be9aa5b11ef7b42c48e
TobiasThomas <tobias.thomas@stud.tu-darmstadt.de>
2018-05-13 20:54:19 +0200

    created a trial of all 32 Bugs (in random order) and optimized imports other small changes

--- DaxLearning.py
+++ DaxLearning.py
@@ -1,7 +1,6 @@
+import random
+from random import randint, shuffle
 from psychopy import visual, core, event, gui, data
-from psychopy.tools.filetools import fromFile, toFile
-from random import randint
-import numpy, random
 
 
 def createBugCharacteristics(amount):
@@ -14,7 +13,6 @@ def createBugCharacteristics(amount):
             chars += random.choice(['0', '1'])
         else:
             chars += 'x'
-    print(chars)
     return chars
 
 
@@ -28,6 +26,19 @@ def isThisADax(bugInfos):
             return False
     return True
 
+def createListOfBugs():
+    allBugs = []
+    for i in range(0,32):
+        binI = createUniformBinary(i)
+        allBugs.append(str(binI))
+    return allBugs
+
+def createUniformBinary(i):
+    binI = bin(i)
+    binI = binI[2:]
+    while len(binI) < 5:
+        binI = '0'+binI
+    return binI
 
 bugCharacteristics = createBugCharacteristics(randint(1, 3))
 
@@ -39,55 +50,57 @@ if dlg.OK == False:
 
 fileName = 'data/'+expInfo['person'] + expInfo['dateStr']
 dataFile = open(fileName + '.csv', 'w')
+dataFile.write('DaxCharacteristics: ,'+bugCharacteristics+'\n')
 dataFile.write('corrAns,bugCharacteristics,DidVPCorr,Time\n')
 
 win = visual.Window([800, 600], monitor='testMonitor', units='deg')
 win.colorSpace = 'rgb255'
 win.color = [255, 255, 255]
 
-instr1 = visual.TextStim(win, pos=[0, +3], text='Hit any key to continue')
+instr1 = visual.TextStim(win, pos=[0, +3], text='Press any key to continue')
 instr2 = visual.TextStim(win, pos=[0, -3], text='Explain Experiment here')
 instr1.draw()
 instr2.draw()
 win.flip()
 event.waitKeys()
 
+
+allBugs = createListOfBugs()
 responseTimer = core.Clock()
 while True:
-    event.clearEvents()
-    bugName = ''
-    for i in range(0, 5):
-        nextElem = random.choice(['0', '1'])
-        bugName += nextElem
-    imagePath = 'BugImages/' + bugName + '.png'
-    bug = visual.ImageStim(win, pos=[0, 0], image=imagePath, size=None, units='pix')
-    bug.draw()
-    win.flip()
-    responseTimer.reset()
-    timeForThisTrial = 0.0
-
-    wasCorrect = None
-    corrAns = isThisADax(bugName)
-    while wasCorrect == None:
-        allKeys = event.waitKeys()
-        for thisKey in allKeys:
-            if thisKey in ['q', 'escape']:
-                dataFile.close()
-                core.quit()
-            elif thisKey in ['n', 'y']:
-                timeForThisTrial = responseTimer.getTime()
-                wasCorrect = (thisKey == 'y' and corrAns) or (thisKey == 'n' and (not corrAns))
+    shuffle(allBugs)
+    for index in range(0,32):
         event.clearEvents()
+        bugName = allBugs[index]
+        imagePath = 'BugImages/' + bugName + '.png'
+        bug = visual.ImageStim(win, pos=[0, 0], image=imagePath, size=None, units='pix')
+        bug.draw()
+        win.flip()
+        responseTimer.reset()
+        timeForThisTrial = 0.0
 
-    dataFile.write('%i,%s,%i,%.4f\n' % (corrAns, bugName, wasCorrect, timeForThisTrial))
-    feedbackText = ''
-    if wasCorrect:
-        feedbackText = 'correct'
-    else:
-        feedbackText = 'wrong'
-    feedback = visual.TextStim(win, pos=[0, -3], text='Your answer was ' + feedbackText)
-    feedback.colorSpace = 'rgb255'
-    feedback.color = [0, 0, 0]
-    feedback.draw()
-    win.flip()
-    core.wait(1.5)
+        wasCorrect = None
+        corrAns = isThisADax(bugName)
+        while wasCorrect == None:
+            allKeys = event.waitKeys()
+            for thisKey in allKeys:
+                if thisKey in ['q', 'escape']:
+                    dataFile.close()
+                    core.quit()
+                elif thisKey in ['n', 'y']:
+                    timeForThisTrial = responseTimer.getTime()
+                    wasCorrect = (thisKey == 'y' and corrAns) or (thisKey == 'n' and (not corrAns))
+            event.clearEvents()
+
+        dataFile.write('%i,%s,%i,%.4f\n' % (corrAns, bugName, wasCorrect, timeForThisTrial))
+        feedbackText = ''
+        if wasCorrect:
+            feedbackText = 'correct'
+        else:
+            feedbackText = 'wrong'
+        feedback = visual.TextStim(win, pos=[0, -3], text='Your answer was ' + feedbackText)
+        feedback.colorSpace = 'rgb255'
+        feedback.color = [0, 0, 0]
+        feedback.draw()
+        win.flip()
+        core.wait(1.5)
