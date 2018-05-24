970ef444c7d57df5a60c5e5a0ae2b56882447635
TobiasThomas <tobias.thomas@stud.tu-darmstadt.de>
2018-05-24 09:13:12 +0200

    improved Version Space Algorith in case of a lot of negative Bugs appear

--- VersionSpace.py
+++ VersionSpace.py
@@ -1,5 +1,6 @@
 class VersionSpace:
-    G = ['xxxxx']
+    G = [[] for _ in range(0,6)]
+    G[0].append('xxxxx')
     S = ''
     numberOfAttributes = -1
 
@@ -13,16 +14,32 @@ class VersionSpace:
             self.spezializeGeneral(bug)
         if self.S != '':
             self.correctMissmatches(self.S)
-        if len(self.G) == 1 and self.G[0] == self.S or self.numOfS() == self.numberOfAttributes:
+        self.removeDoublesFromG()
+        if self.numOfHypo(self.S) == self.numberOfAttributes:
             return self.S
-        self.G = list(set(self.G))  # remove duplicates from G
+        if len(self.G[self.numberOfAttributes]) == 1:
+            return self.G[self.numberOfAttributes][0]
         return False
 
-    def numOfS(self):
+    def removeDoublesFromG(self):
+        i = 0
+        for hList in self.G:
+            if not hList:
+                self.G[i] = []
+            else:
+                self.G[i] = self.removeDuplicates(hList)
+            i += 1
+
+    def removeDuplicates(self,list):
+        seen = set()
+        seenAdd = seen.add
+        return[x for x in list if not (x in seen or seenAdd(x))]
+
+    def numOfHypo(self,hypo):
         num = 0
-        gList = list(self.S)
-        for i in range(0,len(gList)):
-            if gList[i] != 'x':
+        hypoList = list(hypo)
+        for i in range(0,len(hypoList)):
+            if hypoList[i] != 'x':
                 num += 1
         return num
 
@@ -41,14 +58,21 @@ class VersionSpace:
             self.S = newS
 
     def spezializeGeneral(self, bug):
-        newG = []
+        newG = [[] for _ in range(0,6)]
         invBug = self.getInvBug(bug)
-        for hypothesis in self.G:
-            if self.doesHypothesisMatch(hypothesis,bug):
-                newG += self.specializeHypothesis(hypothesis,invBug)
-            else:
-                newG.append(hypothesis)
+        for i in range(0,len(self.G)):
+            for hypothesis in self.G[i]:
+                if self.doesHypothesisMatch(hypothesis,bug):
+                    newHypoList = self.specializeHypothesis(hypothesis,invBug)
+                    for h in newHypoList:
+                        self.addToG(newG,h)
+                else:
+                    self.addToG(newG,hypothesis)
         self.G = newG
+
+    def addToG(self,currG,newHypo):
+        numOfAttributes = self.numOfHypo(newHypo)
+        currG[numOfAttributes].append(newHypo)
 
     def specializeHypothesis(self,hypothesis,bug):
         specHypo = []
@@ -78,10 +102,11 @@ class VersionSpace:
         return invBug
 
     def correctMissmatches(self, reference):
-        newG = []
-        for hypothesis in self.G:
-            if self.doesHypothesisMatch(hypothesis,reference):
-                newG.append(hypothesis)
+        newG = [[] for i in range(0,6)]
+        for i in range(0,len(self.G)):
+            for hypothesis in self.G[i]:
+                if self.doesHypothesisMatch(hypothesis,reference):
+                    newG.append(hypothesis)
         self.G = newG
 
     def doesHypothesisMatch(self,hypothesis,reference):
