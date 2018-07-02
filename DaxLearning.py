2eb2b89823fde66d05f88f18747f77da9084ab61
TobiasThomas <tobias.thomas@stud.tu-darmstadt.de>
2018-07-02 16:34:30 +0200

    final changes for the experiment

--- DaxLearning.py
+++ DaxLearning.py
@@ -42,12 +42,12 @@ def createUniformBinary(i):
     return binI
 
 def intermediateQuestion():
-    questions = {'wings': ['Does Not Matter', 'Yes', 'No'],
-                 'legs': ['Does Not Matter', 'Four', 'Six'],
-                 'antenna': ['Does Not Matter', 'Straight', 'Curly'],
-                 'dots': ['Does Not Matter', 'Small', 'Big'],
-                 'eyes': ['Does Not Matter', 'White', 'Black']}
-    guess = gui.DlgFromDict(questions, title='Guess what a Dax is', sort_keys=False)
+    questions = {'Flügel': ['Spielt keine Rolle', 'Ja', 'Nein'],
+                 'Beine': ['Spielt keine Rolle', 'Vier', 'Sechs'],
+                 'Antenne': ['Spielt keine Rolle', 'gerade', 'gekrümmt'],
+                 'Punkte': ['Spielt keine Rolle', 'klein', 'dick'],
+                 'Augen': ['Spielt keine Rolle', 'Weiß', 'Schwarz']}
+    guess = gui.DlgFromDict(questions, title='Gib deine Hypothese ab', sort_keys=False)
     if guess.OK:
         for key in questions:
             dataFile.write(key + ': ' + questions[key]+', ')
@@ -64,19 +64,22 @@ def checkIfCorrect(dict):
     return False
 
 def translateWordsToChars(word):
-    zeroWords = ['No','Four','Straight','Small','White']
-    oneWords = ['Yes','Six','Curly','Big','Black']
+    zeroWords = ['Nein','Vier','gerade','klein','Weiß']
+    oneWords = ['Ja','Sechs','gekrümmt','dick','Schwarz']
     if word in zeroWords:
         return '0'
     elif word in oneWords:
         return '1'
-    elif word == 'Does Not Matter':
+    elif word == 'Spielt keine Rolle':
         return 'x'
     else:
         print('illegal word')
 
-amountOfAttributes = randint(1, 5)
-bugCharacteristics = createBugCharacteristics(amountOfAttributes)
+def getTestProcedure():
+    possiblities = [[1,3,1,3,4,2,5],[1,3,3,4,2,5,1],[1,3,5,2,1,4,3],[1,3,2,1,5,3,4],[1,3,4,5,3,1,2]]
+    return sample(possiblities,1)[0]
+
+procedure = getTestProcedure()
 
 expInfo = {'person': '', 'dateStr': data.getDateStr()}
 
@@ -86,23 +89,28 @@ if dlg.OK == False:
 
 fileName = 'data/'+expInfo['person'] + expInfo['dateStr']
 dataFile = open(fileName + '.csv', 'w')
-dataFile.write('DaxCharacteristics: ,'+bugCharacteristics+'\n')
+dataFile.write('procedure: '+';'.join(map(str,procedure))+'\n')
 dataFile.write('corrAns,bugCharacteristics,DidVPCorr,Time\n')
 
 win = visual.Window(fullscr=True, monitor='testMonitor', units='pix')
 win.colorSpace = 'rgb255'
 win.color = [255, 255, 255]
+win.mouseVisible = False
 
-instr1 = visual.TextStim(win, pos=[0, +10], text='In the following Experiments you will see Bugs with 5 different attributes.\n'
-                                                  'These are: wings, legs, antenna, dots, eyes.\n'
-                                                  'All of them are binary.\n'
-                                                  'Below you can see 2 examples that show all possible attributes.\n'
-                                                  'All other attributes does not matter.\n\n\n\n'
-                                                  'Press any key to continue.')
+instr1 = visual.TextStim(win, pos=[0, +15], text='In diesem Experiment werden Sie Käfer mit 5 verschiedenen Eigenschaften sehen\n'
+                                                'Die verschiedenen eigenschaften sind: Flügel, Punkte, Beine, Antennen und Augen\n'
+                                                'Alle Eigenschaften gibt es nur in 2 Varianten.\n'
+                                                'Die unterschiedlichen Varianten der Variablen kannst du unten beispielhaft sehen.\n'
+                                                'Es gibt alle Kombinationen der Eigenschaften.'
+                                                'Ihre Aufgabe ist es herauszufinden welche Eigenschaften einen Dax ausmachen'
+                                                'Ein Dax kann beliebig viele Eigenschaften haben.'
+                                                'Dieses Experiment beschäftigt sich mit Lösungsstrategien.'
+                                                'Falls Sie noch Fragen haben stellen sie diese bitte jetzt.\n\n\n\n'
+                                                'Zum Starten drücke eine beliebige Taste.')
 instr1.draw()
-exampleImage1 = visual.ImageStim(win, pos=[-5,-5],image='BugImages/00000.png',units='deg')
+exampleImage1 = visual.ImageStim(win, pos=[-5,-8],image='BugImages/00000.png',units='deg')
 exampleImage1.draw()
-exampleImage2 = visual.ImageStim(win, pos=[+5,-5],image='BugImages/11111.png',units='deg')
+exampleImage2 = visual.ImageStim(win, pos=[+5,-8],image='BugImages/11111.png',units='deg')
 exampleImage2.draw()
 win.flip()
 event.waitKeys()
@@ -111,83 +119,108 @@ vs = VersionSpace()
 solution = False
 
 allBugs = createListOfBugs()
-responseTimer = core.Clock()
-correctRun = False
-didAnswerCorrect = False
-while not(correctRun and didAnswerCorrect):
-    shuffle(allBugs)
-    correctRun = True
-    numCorrect = 0
 
-    for index in range(0,32):
-        event.clearEvents()
-        bugName = allBugs[index]
-        imagePath = 'BugImages/' + bugName + '.png'
-        bug = visual.ImageStim(win, pos=[0, 0], image=imagePath, size=[288,212], units='pix')
-        bug.draw()
-        win.flip()
-        responseTimer.reset()
-        timeForThisTrial = 0.0
+for amountOfAttributes in procedure:
+    bugCharacteristics = createBugCharacteristics(amountOfAttributes)
+    dataFile.write('DaxCharacteristics: ,' + bugCharacteristics + '\n')
+    responseTimer = core.Clock()
+    correctRun = False
+    didAnswerCorrect = False
+    while not(correctRun and didAnswerCorrect):
+        shuffle(allBugs)
+        correctRun = True
+        numCorrect = 0
 
-        wasCorrect = None
-        corrAns = isThisADax(bugName)
-
-        solution = vs.showNewBug(bugName,corrAns)
-        if type(solution) is str:  # version space algorithm found the solution
-            print(solution)
-
-        while wasCorrect == None:
-            allKeys = event.waitKeys()
-            for thisKey in allKeys:
-                if thisKey in ['q', 'escape']:
-                    dataFile.close()
-                    core.quit()
-                elif thisKey in ['n', 'y']:
-                    timeForThisTrial = responseTimer.getTime()
-                    wasCorrect = (thisKey == 'y' and corrAns) or (thisKey == 'n' and (not corrAns))
+        for index in range(0,32):
             event.clearEvents()
+            bugName = allBugs[index]
+            imagePath = 'BugImages/' + bugName + '.png'
+            bug = visual.ImageStim(win, pos=[0, 0], image=imagePath, size=[288,212], units='pix')
+            bug.draw()
+            win.flip()
+            responseTimer.reset()
+            timeForThisTrial = 0.0
 
-        dataFile.write('%i,%s,%i,%.4f\n' % (corrAns, bugName, wasCorrect, timeForThisTrial))
-        feedbackText = ''
-        feedbackText2 = ''
-        if not corrAns:
-            feedbackText2 = 'NOT '
-        feedbackRect = visual.Rect(win, lineWidth=25.0, size=[1600,1200])
-        if wasCorrect:
-            numCorrect += 1
-            correct = sound.Sound('sounds/correctAnswer.ogg')
-            correct.play()
-            feedbackRect.lineColor = 'green'
-            feedbackText = 'correct'
-        else:
-            correctRun = False
-            wrong = sound.Sound('sounds/wrongAnswer.ogg')
-            wrong.play()
-            feedbackRect.lineColor = 'red'
-            feedbackText = 'wrong'
-        feedback = visual.TextStim(win, pos=[0, -10], text='Your answer was ' + feedbackText+
-                                                           '\n This was '+feedbackText2+' a Dax')
-        feedback.colorSpace = 'rgb255'
-        feedback.color = [0, 0, 0]
-        feedback.draw()
-        feedbackRect.draw()
+            wasCorrect = None
+            corrAns = isThisADax(bugName)
+
+            solution = vs.showNewBug(bugName,corrAns)
+
+            while wasCorrect == None:
+                allKeys = event.waitKeys()
+                for thisKey in allKeys:
+                    if thisKey in ['q', 'escape']:
+                        dataFile.close()
+                        core.quit()
+                    elif thisKey in ['n', 'y']:
+                        wasCorrect = (thisKey == 'y' and corrAns) or (thisKey == 'n' and (not corrAns))
+                event.clearEvents()
+
+            dataFile.write('%i,%s,%i,' % (corrAns, bugName, wasCorrect))
+            feedbackText = ''
+            feedbackText2 = ''
+            if not corrAns:
+                feedbackText2 = 'KEIN'
+            else:
+                feedbackText2 = 'EIN'
+            feedbackRect = visual.Rect(win, lineWidth=25.0, size=[1600,1200])
+            if wasCorrect:
+                numCorrect += 1
+                correct = sound.Sound('sounds/correctAnswer.ogg')
+                correct.play()
+                feedbackRect.lineColor = 'green'
+                feedbackText = 'richtig'
+            else:
+                correctRun = False
+                wrong = sound.Sound('sounds/wrongAnswer.ogg')
+                wrong.play()
+                feedbackRect.lineColor = 'red'
+                feedbackText = 'falsch'
+            feedback = visual.TextStim(win, pos=[0, -100], text='Deine Antwort war ' + feedbackText+
+                                                               '\nDas war '+feedbackText2+' Dax')
+            feedback.colorSpace = 'rgb255'
+            feedback.color = [0, 0, 0]
+            feedback.draw()
+            feedbackRect.draw()
+            bug.pos = [0,100]
+            bug.draw()
+            win.flip()
+            event.clearEvents()
+            core.wait(0.5)
+            feedbackTimer = core.CountdownTimer(2.5)
+            while feedbackTimer.getTime() > 0:
+                keysPressed = event.getKeys()
+                if 'space' in keysPressed:
+                    break
+                else:
+                    continue
+            timeForThisTrial = responseTimer.getTime()
+            dataFile.write('%.4f\n'%(timeForThisTrial))
+
+
+
+        fb = visual.TextStim(win, pos=[0,0], text=str(numCorrect)+' deiner letzten 32 Antworten waren korrekt\n\n'
+                                                             'Beliebige Taste drücken zum Fortsetzen.')
+        fb.colorSpace = 'rgb255'
+        fb.color = [0,0,0]
+        fb.draw()
         win.flip()
-        core.wait(3)
+        event.waitKeys()
 
-    fb = visual.TextStim(win, pos=[0,0], text=str(numCorrect)+' of your last 32 answers were correct.\n\n'
-                                                         'Press any key to continue.')
-    fb.colorSpace = 'rgb255'
-    fb.color = [0,0,0]
-    fb.draw()
+        win.winHandle.set_fullscreen(False)
+        win.winHandle.set_visible(False)
+        didAnswerCorrect = intermediateQuestion()
+        win.winHandle.set_fullscreen(True)
+        win.winHandle.set_visible(True)
+        win.flip()
+
+    intermediateFeedback = visual.TextStim(win, pos=[0,0], text='Super! Du hast dieses Rätsel gelöst.\n'
+                                                                'Wenn du dich bereit für das nächste Rätsel fühlst drücke eine beliebige Tasten zum Fortfahren')
+    intermediateFeedback.colorSpace = 'rgb255'
+    intermediateFeedback.color = [0,0,0]
+    intermediateFeedback.draw()
     win.flip()
     event.waitKeys()
-
-    win.winHandle.set_fullscreen(False)
-    win.winHandle.set_visible(False)
-    didAnswerCorrect = intermediateQuestion()
-    win.winHandle.set_fullscreen(True)
-    win.winHandle.set_visible(True)
-    win.flip()
 
 dataFile.close()
 core.quit()
